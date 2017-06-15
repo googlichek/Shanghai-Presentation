389cbae9085e57dc78bcaa72be6b3a693bbdcc7b
Goga Akobiya <googlichek.one@gmail.com>
2017-06-15 18:56:34 +0300

    shanghai-pres: In the middle of creating of an app backbone.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+	public float Speed = 10;
+	public Vector3 Direction = Vector3.up;
+
+	void Update()
+	{
+		transform.Rotate(Direction * Speed * Time.deltaTime);
+	}
+}
